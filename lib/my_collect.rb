e040f9fa818a68709f44d66518a561c29fff080f
Eliot Howes <ejhowes@gmail.com>
2019-05-26 17:27:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+    while counter < collection.length
+      new_collection.push(yield(collection[counter]))
+     counter += 1
+    end
+    new_collection
+end
